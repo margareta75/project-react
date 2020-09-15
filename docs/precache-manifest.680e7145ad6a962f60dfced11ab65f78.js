51511bea03516c45cd920215500c7598438aba43
margareta75 <margareta.xx@gmail.com>
2020-09-15 10:06:25 +0200

    Change from build to docs

---
+++ docs/precache-manifest.680e7145ad6a962f60dfced11ab65f78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d432e0085425f438b100b892200d29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91207636029412babf13",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "04075cb94a7c004d3823",
+    "url": "/static/js/2.082b8f26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.082b8f26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91207636029412babf13",
+    "url": "/static/js/main.9e08bc02.chunk.js"
+  },
+  {
+    "revision": "db8be71d452dcba867cd",
+    "url": "/static/js/runtime-main.7a44f2df.js"
+  }
+]);
