6a447f9c03d994feedb8df4a9837b0c911fa05b6
margareta75 <margareta.xx@gmail.com>
2020-09-15 19:40:58 +0200

    Changes to docs

--- docs/precache-manifest.5a1f85a8ccfe0ca7d1139854f35a363b.js
+++ docs/precache-manifest.5a1f85a8ccfe0ca7d1139854f35a363b.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a6a1c908a85195cce860a5b40019c740",
+    "revision": "ba43089cedd91b296ee8342a82ba857d",
     "url": "/project-react/index.html"
   },
   {
-    "revision": "3fc1edc52e5b26e6d011",
+    "revision": "3df940a8ca892858ec5b",
     "url": "/project-react/static/css/main.27fd15f6.chunk.css"
   },
   {
-    "revision": "04075cb94a7c004d3823",
-    "url": "/project-react/static/js/2.082b8f26.chunk.js"
+    "revision": "ade21a90660e56c982ba",
+    "url": "/project-react/static/js/2.e2f8d85a.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/project-react/static/js/2.082b8f26.chunk.js.LICENSE.txt"
+    "url": "/project-react/static/js/2.e2f8d85a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3fc1edc52e5b26e6d011",
-    "url": "/project-react/static/js/main.eea75b87.chunk.js"
+    "revision": "3df940a8ca892858ec5b",
+    "url": "/project-react/static/js/main.ef91aa27.chunk.js"
   },
   {
     "revision": "316e1c51ac167954acad",
