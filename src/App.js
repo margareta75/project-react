e827ca927282fd629832f3204efff3a2d59b31f3
margareta75 <margareta.xx@gmail.com>
2020-09-15 15:05:34 +0200

    AddTodo function and more

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Todos from "./Todos";
 import Header from "./Header";
+import AddTodo from "./AddTodo";
 import "./App.css";
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
     ],
   };
 
+  // Toggle completed
   markComplete = (id) => {
     this.setState({
       todos: this.state.todos.map((todo) => {
@@ -25,16 +27,29 @@ class App extends Component {
     console.log("clicked");
   };
 
+  // Delete Todo
   deleteTodo = (id) => {
     console.log("deleted todo: ", id);
     const newState = this.state.todos.filter((todo) => todo.id !== id);
     this.setState({ todos: newState });
   };
 
+  //Add Todo
+  addTodo = (title) => {
+    console.log(title);
+    const newTodo = {
+      id: 4,
+      title: title,
+      completed: false,
+    };
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  };
+
   render() {
     return (
       <div className="App">
         <Header />
+        <AddTodo addTodo={this.addTodo} />
         <Todos
           todos={this.state.todos}
           markCompletePropApp={this.markComplete}
